9273558c386e793c16fe6c913b5ff57ac5364cfa
FilipBirkfeldt <filip.birkfeldt@gmail.com>
2020-09-01 14:31:45 +0200

    fafqf

--- clock/src/ClockMain.java
+++ clock/src/ClockMain.java
@@ -2,18 +2,24 @@ import clock.AlarmClockEmulator;
 import clock.io.ClockInput;
 import clock.io.ClockInput.UserInput;
 import clock.io.ClockOutput;
+import clock.io.MonitorThreadHandler;
 
 import java.time.LocalTime; 
 import java.util.concurrent.Semaphore;
 
 public class ClockMain {
 	
-	// 1.  Trådar - Generell_TidsTråd, AlarmTråd
-	// 2.  
+	// 1.  Trådar - Generell_TidsTråd, AlarmTråd, UserInputThread
+	// 2.  Datan som delas - Alarmet 
+	// 3.  Check 
+	// - Alarmet, kolla på tiden & userInput (Get/set) 
+	// - I Monitor
+	// 4. Main-metoden 
+	// 5. Semafore behövs för att sätta tid & alarm. 
 	
     public static void main(String[] args) throws InterruptedException {
         
-    	// Emulator - GUI 
+    	// Emulator - GUI (fick den här koden)  
     	AlarmClockEmulator emulator = new AlarmClockEmulator();
         ClockInput  in  = emulator.getInput();
         ClockOutput out = emulator.getOutput();
@@ -23,7 +29,9 @@ public class ClockMain {
         
         
         // Tråd för att låta tiden rulla 
-        Thread timeThread = new Thread(Monitor, out); 
+        MonitorThreadHandler disp_time = new MonitorThreadHandler(out);
+        out.displayTime(currentTime, currentTime, currentTime);
+        
         
         
         
@@ -31,6 +39,7 @@ public class ClockMain {
 
         
         // Del I1
+        // Semaphore - acquire() & release()  ( in-user input) 
         Semaphore sem = in.getSemaphore();
         
         while (true) {
---
+++ clock/src/clock/io/MonitorThreadHandler.java
@@ -0,0 +1,31 @@
+package clock.io;
+
+import java.util.concurrent.Semaphore; 
+
+
+public class MonitorThreadHandler extends Thread {
+	
+	private ClockOutput clockOutput; 
+	private Semaphore mutex = new Semaphore(1); //Mutex - lås race condition kapplöpning
+	private int time; 
+	private int alarmTime = 0; 
+	private boolean alarm = false; 
+
+
+	
+	public MonitorThreadHandler(ClockOutput clockOutput) {
+		this.clockOutput = clockOutput; 
+	}
+	
+	private int retrieveTime() {
+		return time; 
+	}
+	
+	public void run() {
+		
+	}
+	
+	
+	
+	
+}
