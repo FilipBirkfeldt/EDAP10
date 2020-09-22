fb104a846f0b6df686e405a037be6337b43e4d61
FilipBirkfeldt <filip.birkfeldt@gmail.com>
2020-09-22 17:36:05 +0200

    klara med labben

--- lift/src/Main.java
+++ lift/src/Main.java
@@ -13,13 +13,20 @@ public class Main {
 		LiftThread lift = new LiftThread(view, mon);
 		lift.start();
 
-		for (int i = 0; i < 20; i++) {
+		for (int i = 0; i < 40; i++) {
 			
 			Passenger pass = view.createPassenger();
 			PersonThread pers = new PersonThread(pass, mon);
 			pers.start();
-			Thread.sleep(rand.nextInt(5000));
+			Thread.sleep(rand.nextInt(2000));
 		}
 
 	}
 }
+// R1. för personerna och för hissen. Gemensamma attribut 
+// R2. Med if så kollas de bara första veckan - då kollas inte tråden igen 
+// R3. En är uppfylld men inte båda. passEntry & passExit skulle kunna vara en metod. Fråga om hjälp
+// R4. För moveLift() tar för lång tid
+// R5. modify - notifyAll()
+// R6. wait() lägger sig utanför monitorn. Synchronize o monitor hör ihop. 
+
